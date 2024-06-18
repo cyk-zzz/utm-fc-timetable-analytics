cd4ef1e077becac65e678f519339709e403e0174
yk <62372030+cyk-zzz@users.noreply.github.com>
2024-06-19 01:18:51 +0800

    Add doughnut chart percentage, column chart

--- app/config/ChartJsProvider.config.js
+++ app/config/ChartJsProvider.config.js
@@ -9,11 +9,37 @@
 
     function ChartJsProvider(ChartJsProvider) {
         ChartJsProvider.setOptions({
-            chartColors: ['#FF5252', '#FF8A80'],
+            chartColors: ['#e60049', '#0bb4ff', '#50e991'],
             responsive: true,
             legend: {
                 display: true,
-                position: 'top',
+                position: 'right',
+            },
+        });
+
+        ChartJsProvider.setOptions('doughnut', {
+            tooltips: {
+                callbacks: {
+                    label: (item, data) => {
+                        let labelName = data.labels[item.index];
+                        let total = data.datasets[item.datasetIndex].data.reduce((sum, a) => sum + a, 0);
+                        let count = data.datasets[item.datasetIndex].data[item.index];
+                        let percent = (count / total) * 100;
+                        return `${labelName}: ${Number(percent).toFixed(2)}%`;
+                    }
+                }
+            },
+        });
+
+        // Configure all line charts
+        ChartJsProvider.setOptions('horizontalBar', {
+            scales: {
+                xAxes: [{
+                    stacked: true
+                }],
+                yAxes: [{
+                    stacked: true
+                }]
             }
         });
 
